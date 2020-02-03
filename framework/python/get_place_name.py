43228a4f362fdc450572e9400b0de51fa549ff6e
GusRiva <gusfer@gmail.com>
2020-02-03 17:48:33 +0100

    GND gets results

--- framework/python/get_place_name.py
+++ framework/python/get_place_name.py
@@ -8,47 +8,72 @@ import tkinter as tk
 #     text = json.dumps(obj, sort_keys=True, indent=4)
 #     print(text)
 
-# response = requests.get("https://lobid.org/gnd/search?q=waldorf&filter=type:TerritorialCorporateBodyOrAdministrativeUnit&format=json")
-# print(response.status_code)
-# resultDict = {}
-#
-# for item in response.json()['member']:
-#     resultDict[item['preferredName']]= item['gndIdentifier']
 
-def show_entry_fields():
-    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+
+def sucheGND(searchTerm, v):
+    oldRadios = radioFrame.winfo_children()
+    for olr in oldRadios:
+        olr.destroy()
+    print(searchTerm)
+    response = requests.get(
+        "https://lobid.org/gnd/search?q=" + searchTerm + "&filter=type:TerritorialCorporateBodyOrAdministrativeUnit&format=json")
+    print(response.status_code)
+    searchResult = []
+
+    for item in response.json()['member']:
+        searchResult.append( (item['preferredName'], item['gndIdentifier']) )
+
+    print(searchResult)
+
+
+    for val, tup in enumerate(searchResult):
+        tk.Radiobutton(radioFrame,
+                       text= tup[0],
+                       # padx=20,
+                       variable=v,
+                       # command=ShowChoice,
+                       value=tup[1]).grid(row=1 + val, column=1, sticky=tk.W)
+
+    v.set(searchResult[0][1])
+
 
 master = tk.Tk()
+v = tk.IntVar()
+master.geometry("600x600") #Width x Height
 tk.Label(master,
-         text="First Name").grid(row=0)
-tk.Label(master,
-         text="Last Name").grid(row=1)
+     text="Suche nach: ").grid(row=0,
+                               padx=20,
+                               pady=20)
 
-e1 = tk.Entry(master)
-e2 = tk.Entry(master)
+def get(event):
+    sucheGND(event.widget.get(), v)
 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+e1 = tk.Entry(master, width=50)
+e1.bind('<Return>', get )
+e1.focus_set()
 
-res = tk.Label(master)
-res.grid(row=0, column=1)
-
-tk.Button(master,
-          text='Quit',
-          command=master.quit).grid(row=3,
-                                    column=0,
-                                    sticky=tk.W,
-                                    pady=4)
+e1.grid(row=0, column=2,
+        columnspan=2,
+        pady= 4,
+        sticky=tk.W)
 
 
+tk.Button(master, text='Search'
+          ,command= lambda: sucheGND(e1.get(),v)
+          ).grid(row=0, column=4,
+                 padx=10,
+                 sticky=tk.W)
+tk.Button(master, text='Add'
+          # ,command= lambda: sucheGND(e1.get())
+          , state= 'disabled').grid(row=0, column=5, padx=10, sticky=tk.W)
 
-tk.Button(master,
-          text='Show', command=show_entry_fields).grid(row=3,
-                                                       column=1,
-                                                       sticky=tk.W,
-                                                       pady=4)
+radioFrame = tk.Frame(master)
+radioFrame.grid(row=1, column=2, sticky=tk.W)
+
+
 
 tk.mainloop()
+
 
 
 
